190fbd4a79beb8940309715cc1c6f6b599bf8021
mubbashir shakil <mshakil74@gmail.com>
2023-06-14 01:39:56 +0500

    Adding API project and task

---
+++ codeup-alchemy-assessment-api/Helper/Request.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace codeup_alchemy_assessment_api
+{
+    public class RequestClient
+    {
+        
+
+        public static string GetToken(string jsonString)
+        {
+            var json = JsonConvert.DeserializeObject(jsonString);
+
+            var token = JToken.Parse(json.ToString()).SelectToken("[4].address.city").ToString();
+
+            return token;
+
+        }
+    }
+}
---
+++ codeup-alchemy-assessment-api/Tests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Authenticators;
+
+
+namespace codeup_alchemy_assessment_api
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void ApiTest() 
+        {
+            var restClient = new RestClient("https://jsonplaceholder.typicode.com/");
+            var restRequest = new RestRequest("users/", Method.Get);
+
+
+            RestResponse response = restClient.Execute(restRequest);
+            var content = response.Content;
+
+            int responseCode = Convert.ToInt32(response.StatusCode);
+            Assert.AreEqual(200, responseCode, "Status code is not 200");
+
+            Assert.AreEqual("Roscoeview", RequestClient.GetToken(content), "City is not same as expected");
+        }
+    }
+}
